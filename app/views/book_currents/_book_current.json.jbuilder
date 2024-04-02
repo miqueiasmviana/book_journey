7ef715bc0ff1b712d80ed5ff8395a08a6a59da2a
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-02 11:44:06 -0300

    criando 2o CRUD

---
+++ app/views/book_currents/_book_current.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! book_current, :id, :current_type, :created_at, :updated_at
+json.url book_current_url(book_current, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_01_231347) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_144303) do
+  create_table "book_currents", force: :cascade do |t|
+    t.string "current_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "cover"
     t.string "title"
