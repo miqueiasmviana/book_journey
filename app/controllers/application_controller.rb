ef4e2c30b18f9764f5204583b9ff9b2f3377374b
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-03 13:27:27 -0300

    adicionando traduções e definindo na aplication controller a rota da tradução

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
+  before_action :set_locale 
   
   def set_locale
     if params[:locale]
       cookies[:locale] = params[:locale]
+      redirect_to params[:url] || root_path
     end
     if cookies[:locale]
       if I18n.locale != cookies[:locale]
